26629af180967798cf0cc3266d3309bd090a9a66
Cédric Felizard <cedric@felizard.fr>
2012-02-07 17:08:49 +0000

    Remove test script

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for ruby in ree 1.8.7 1.9.2 1.9.3; do
-    rvm --create $ruby@omniconf
-    echo "Running specs with $(rvm current)..."
-    bundle check || bundle
-    rake spec || exit 1
-done
-
