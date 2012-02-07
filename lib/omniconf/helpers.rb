ea648d747b6e7a2ee35c3e1039970d112de69d01
Cédric Felizard <cedric@felizard.fr>
2012-02-07 15:47:24 +0000

    Fix recursive_stringify_keys! for Ruby 1.9

--- lib/omniconf/helpers.rb
+++ lib/omniconf/helpers.rb
@@ -1,10 +1,11 @@
 class Hash
   # Recursive version of Rails' stringify_keys!
   def recursive_stringify_keys!
-    self.each do |k, v|
-      self[k.to_s] = self.delete(k) unless k.is_a? String
-      v.recursive_stringify_keys! if v.is_a? Hash
+    self.keys.each do |key|
+      self[key.to_s] = self.delete(key) unless key.is_a? String
+      self[key.to_s].recursive_stringify_keys! if self[key.to_s].is_a? Hash
     end
+    self
   end
 
   def recursive_merge! with_hash
